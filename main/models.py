baa80d0c610361d1340ea678254bb698111ed597
Andy Tran <ajtran@umich.edu>
2015-08-06 00:39:30 -0400

    'made model, i am the slowest programmer in all the land'

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Spot(models.Model):
+	name = models.CharField(max_length=20)
+	description = models.TextField()
+	lon = models.FloatField()
+	lat = models.FloatField()
